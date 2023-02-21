449722c8610af3c9ad04655345dc5c7eb7378005
adamslucas35 <adamslucas35@gmail.com>
2023-02-20 22:56:43 -0700

    inherited inhouse and outsources from part.java and created the appropiate getter and setters

--- src/main/java/lucas/inventory/model/InHouse.java
+++ src/main/java/lucas/inventory/model/InHouse.java
@@ -1,4 +1,38 @@
 package lucas.inventory.model;
 
-public class InHouse {
+public class InHouse extends Part{
+
+    //fields
+    private int machineId;
+    /**
+     * Constructor to create new part.
+     *
+     * @param id    initialize part id
+     * @param name  initialize part name
+     * @param price initialize part price
+     * @param stock initialize part stock
+     * @param min   initialize min inventory
+     * @param max   initialize max inventory
+     * @param machineId initialize machine id
+     */
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId)
+    {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    /**
+     * @return Gets machineId
+     * */
+    public int getMachineId()
+    {
+        return machineId;
+    }
+    /**
+     * @param machineId sets the machineId
+     * */
+    public void setMachineId(int machineId)
+    {
+        this.machineId = machineId;
+    }
 }
--- src/main/java/lucas/inventory/model/OutSourced.java
+++ src/main/java/lucas/inventory/model/OutSourced.java
@@ -1,4 +1,38 @@
 package lucas.inventory.model;
 
-public class OutSourced {
+public class OutSourced extends Part
+{
+    //fields
+    private String companyName;
+    /**
+     * Constructor to create new part.
+     *
+     * @param id    initialize part id
+     * @param name  initialize part name
+     * @param price initialize part price
+     * @param stock initialize part stock
+     * @param min   initialize min inventory
+     * @param max   initialize max inventory
+     * @param companyName initialize companyName
+     */
+    public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName)
+    {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }
+
+    /**
+     * @return Gets companyName
+     * */
+    public String getCompanyName()
+    {
+        return companyName;
+    }
+    /**
+     * @param companyName sets companyName
+     * */
+    public void setCompanyName(String companyName)
+    {
+        this.companyName = companyName;
+    }
 }
